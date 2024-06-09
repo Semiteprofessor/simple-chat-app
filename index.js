4325fce550cb21607c8b3058f558459acb92867e
Taiwo Olapade <semiteprofessor@gmail.com>
2024-06-09 19:43:54 +0100

    Commit

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const displayRoutes = require("express-routemap");
-const io = require("socket.io");
 const path = require("path");
 const port = process.env.PORT;
 
@@ -16,18 +15,34 @@ const userRoute = require("./routes/user.route");
 app.use("/api/v1/users", userRoute);
 
 // app.use(express.urlencoded({ extended: true }));
-// app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+const server = app.listen(port, (err) => {
+  if (err) {
+    console.error("Failed to start server:", err);
+  } else {
+    console.log(`Server is running at http://localhost:${port}`);
+  }
+});
+
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
 });
 
 io.on("connection", (socket) => {
-  console.log("Client connected" + socket.id);
-})
+  console.log(`Client connected: ${socket.id}`);
+
+  socket.on("disconnect", () => {
+    console.log(`Client disconnected: ${socket.id}`);
+  });
+});
 
 // sequelize
 //   .authenticate()
