4445b9f5ff446deb65b24bf780f570d1bb2ab86b
Taiwo Olapade <semiteprofessor@gmail.com>
2024-06-15 11:35:03 +0100

    Commit

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const appRoute = require("express-routemap");
+const path = require("path");
 const port = process.env.PORT;
 
 const sequelize = require("./config/db.config");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
 sequelize
   .authenticate()
