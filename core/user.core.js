ad8b5790803b17f1b0691e2a348d4ce0364b78ed
Taiwo Olapade <semiteprofessor@gmail.com>
2024-06-17 11:02:10 +0100

    Commit

---
+++ core/user.core.js
@@ -0,0 +1,76 @@
+require("dotenv").config();
+
+const jwt = require("jsonwebtoken");
+const {
+  createUserValidation,
+  loginValidation,
+} = require("../validations/user.validation");
+const UserModel = require("../models/User.model");
+const UserOtpModel = require("../models/Otp.model");
+const {
+  hashPassword,
+  generateOtp,
+  comparePasswords,
+} = require("../utils/_helper.util");
+const { v4: uuidv4 } = require("uuid");
+
+const createNewUser = async (req, res) => {
+  const { error } = createUserValidation(req.body);
+  if (error !== undefined)
+    return res
+      .status(400)
+      .json({ status: true, error: error.details[0].message });
+
+  try {
+    const { username, firstName, lastName, email, password, phone } = req.body;
+    if (!username || !firstName || !lastName || !email || !password || !phone) {
+      return res
+        .status(400)
+        .json({ status: false, error: "All fields are required" });
+    }
+    const isAlreadyExist = await UserModel.findAll({
+      attributes: ["email", "phone"],
+      where: {
+        $or: [{ email }, { phone }],
+      },
+    });
+    if (isAlreadyExist.length > 0) {
+      return res
+        .status(400)
+        .json({ status: false, error: "Email or phone already exists" });
+    }
+
+    const { hash, salt } = await hashPassword(password);
+    const userId = uuidv4();
+    await UserModel.create({
+      userId,
+      username,
+      firstName,
+      lastName,
+      email,
+      password_hash: hash,
+      password_salt: salt,
+      phone,
+      isActive: true,
+    });
+
+    const _otp = generateOtp(6);
+    const dataToInsert = {
+      otp_id: userId,
+      otp: _otp,
+      email_or_phone: email,
+      otp_expiry: new Date(Date.now() + 60 * 1000),
+    };
+    await UserOtpModel.create(dataToInsert);
+
+    // Send OTP to the user via email or SMS
+    // await sendEmailOrSms(email, `Your OTP is ${_otp}`);
+
+    res
+      .status(201)
+      .json({ status: true, message: "User created successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ status: false, error: "Internal Server Error" });
+  }
+};
